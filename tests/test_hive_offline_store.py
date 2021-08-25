5c5be9b505b94386acfcef99be50e08d3fca74ab
benn <benn@thenetcircle.com>
2021-08-25 17:28:58 +0800

    clean up legacy tests

--- tests/test_hive_offline_store.py
+++ tests/test_hive_offline_store.py
@@ -212,30 +212,6 @@ def get_expected_training_df(
     return expected_df
 
 
-# @with_cursor
-# def test_upload_entity_df(cursor):
-#     start_date = datetime.now().replace(microsecond=0, second=0, minute=0)
-#     (_, _, _, entity_df, _,) = generate_entities(start_date, False)
-
-#     # Test upload entity_df to Hive
-#     table_name_1 = offline_utils.get_temp_entity_table_name()
-#     schema_1 = offline_store._upload_entity_df_and_get_entity_schema(
-#         table_name_1, entity_df, cursor
-#     )
-#     cursor.execute_async(f"SELECT * FROM {table_name_1}")
-#     new_entity_df_1 = offline_store.HiveRetrievalJob(cursor).to_df()
-#     assert len(entity_df) == len(new_entity_df_1)
-
-#     # Test upload by Query
-#     table_name_2 = offline_utils.get_temp_entity_table_name()
-#     schema_2 = offline_store._upload_entity_df_and_get_entity_schema(
-#         table_name_2, f"SELECT * FROM {table_name_1}", cursor
-#     )
-#     cursor.execute_async(f"SELECT * FROM {table_name_2}")
-#     new_entity_df_2 = offline_store.HiveRetrievalJob(cursor).to_df()
-#     assert new_entity_df_2.equals(new_entity_df_1)
-
-
 @pytest.mark.parametrize(
     "provider_type", ["local"],
 )
